1f43d69037f4f062e7e71cf1a44f7fa5a2c8a78a
shinystar-0314 <shinystar0314@gmail.com>
2016-09-30 11:44:35 +0300

    Fixed issue when sent nil callbackId to commandDelegate

--- src/ios/SafariViewController.m
+++ src/ios/SafariViewController.m
@@ -30,18 +30,18 @@
   bool readerMode = [options[@"enterReaderModeIfAvailable"] isEqual:[NSNumber numberWithBool:YES]];
   self.animated = [options[@"animated"] isEqual:[NSNumber numberWithBool:YES]];
   self.callbackId = command.callbackId;
-  
+
   vc = [[SFSafariViewController alloc] initWithURL:url entersReaderIfAvailable:readerMode];
   vc.delegate = self;
 
   bool hidden = [options[@"hidden"] isEqualToNumber:[NSNumber numberWithBool:YES]];
   if (hidden) {
     vc.view.userInteractionEnabled = NO;
-    vc.view.alpha = 0.0;
+    vc.view.alpha = 0.05;
     [self.viewController addChildViewController:vc];
     [self.viewController.view addSubview:vc.view];
     [vc didMoveToParentViewController:self.viewController];
-    vc.view.frame = CGRectZero;
+    vc.view.frame = CGRectMake(0.0, 0.0, 0.5, 0.5);
   } else {
     if (self.animated) {
       // note that Apple dropped support for other animations in iOS 9.2 or 9.3 in favor of a slide-back gesture
@@ -49,7 +49,7 @@
     }
     [self.viewController presentViewController:vc animated:self.animated completion:nil];
   }
-  
+
   NSString *tintColor = options[@"tintColor"];
   NSString *controlTintColor = options[@"controlTintColor"];
   NSString *barColor = options[@"barColor"];
@@ -123,7 +123,7 @@
  */
 - (void)safariViewControllerDidFinish:(SFSafariViewController *)controller {
   if (self.callbackId != nil) {
-    NSString * cbid = self.callbackId;
+    NSString * cbid = [self.callbackId copy];
     self.callbackId = nil;
     CDVPluginResult * pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:@{@"event":@"closed"}];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:cbid];
@@ -141,16 +141,6 @@
     [pluginResult setKeepCallback:[NSNumber numberWithBool:YES]];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
   }
-}
-
-- (NSArray<UIActivity *> *)safariViewController:(SFSafariViewController *)
-              controller activityItemsForURL:(NSURL *)URL
-              title:(nullable NSString *)title {
-
-    if(self.activityItemProvider)
-        return [self.activityItemProvider safariViewController:controller activityItemsForURL:URL title:title];
-    else
-        return nil;
 }
 
 @end
